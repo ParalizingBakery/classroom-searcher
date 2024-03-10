bedfa9b8b343bfd6c4ab61c6b2052bdac3cf029f
Wuttphat <matser@Wuttiphats-MacBook-Pro.local>
2024-03-10 19:00:13 +0700

    Made initializing host button a separate function

--- popup/permissions.js
+++ popup/permissions.js
@@ -1,24 +1,34 @@
-let button = document.getElementById("permission-button")
-
-if (button === null) {
-    throw new Error("element #permission-button not found")
+function main() {
+    initHostButton()
 }
 
-const required = {
-    origins:["https://classroom.google.com/*"]
-}
-
-let haveRequired = false
-
-button.addEventListener('click', () => {
-    if (haveRequired) {return}
-    browser.permissions.request(required)
-})
-
-button.addEventListener("click", async () => {
-    if (await browser.permissions.contains(required)) {
-        button.innerHTML = "Already Have Permissions"
-        haveRequired = true
-        return
+function initHostButton () {
+    //Initial variables
+    const PERMISSION_BUTTON_ID = "permission-button"
+    const button = document.getElementById(PERMISSION_BUTTON_ID)
+    if (!button) {
+        throw new Error(`Element with ID ${PERMISSION_BUTTON_ID} not found`)
     }
-})
+    const required = {
+        origins:["https://classroom.google.com/*"]
+    }
+    let haveRequired = false
+
+    //Let the user know if they have perms
+    button.addEventListener("click", async () => {
+        if (await browser.permissions.contains(required)) {
+            button.innerHTML = "Already Have Permissions"
+            haveRequired = true
+            return
+        }
+    })
+
+    //Requesting permissions requires a User action handler.
+    //They aren't allowed to be async, even if the function needs await to get a return value 
+    button.addEventListener("click", () => {
+        if (haveRequired) {return}
+        browser.permissions.request(required)
+    })
+}
+
+main()
