f316238d101d40018fdf1841d086d7a565ec5763
Wuttphat <matser@Wuttiphats-MacBook-Pro.local>
2024-03-13 20:47:13 +0700

    comments about how to use browser-polyfill

--- browser-polyfill.js
+++ browser-polyfill.js
@@ -1,4 +1,5 @@
 //Polyfill browser for chrome. MV3 chrome supports promises
+//Run this file/code before using browser namespace.
 if(typeof browser == "undefined") {
   globalThis.browser = chrome
 }
